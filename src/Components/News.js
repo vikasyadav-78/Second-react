19acb3232ea12f31792e41addd8b485c379398f8
yadavvikas787840@gmail.com <yadavvikas787840@gmail.comcls>
2025-04-17 15:21:37 +0530

    data access

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react'
+import NewsIteam from './NewsIteam'
 
 export class News extends Component {
+  articles = [
+        {
+            "source": {
+                "id": "news24",
+                "name": "News24"
+            },
+            "author": "Khanyiso Tshwaku",
+            "title": "Teen sensation Lhuan-dre Pretorius at it again, continues rise with title-winning Titans ton",
+            "description": "As South African cricket has discovered over the 2024/25 season, there are many strings to Lhuan-dre Pretorius' bow, but saving a 4-Day game clearly wasn't the one that was budgeted for.",
+            "url": "https://www.news24.com/sport/cricket/teen-sensation-lhuan-dre-pretorius-at-it-again-continues-rise-with-title-winning-titans-ton-20250415",
+            "urlToImage": "https://cdn.24.co.za/files/Cms/General/d/12869/abc06335510744efa3ed274066783b92.jpg",
+            "publishedAt": "2025-04-15T11:05:05",
+            "content": "As South African cricket has discovered over the 2024/25 season, there are many strings to Lhuan-dre Pretorius' bow, but saving a 4-Day game clearly wasn't the one that was budgeted for."
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+            "publishedAt": "2020-04-27T11:41:47Z",
+            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+        },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+            "publishedAt": "2020-03-30T15:26:05Z",
+            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+        }
+    ]
+  constructor(){
+    super();
+    this.state = {
+      articles : this.articles,
+      loading : false
+    }
+
+  }
   render() {
     return (
-      <div>
-        
+      <div className='container my-3'>
+        <h2>NewsApp - Today Top HeadLines</h2>
+        <div className='row'>
+          {this.articles.map((data) =>{
+          return <div className='col-sm-12 col-md-6 col-lg-4' key={data.url}>
+            <NewsIteam title={data.title.slice(0, 40)} descripation={data.description.slice(0,88)}  imageUrl={data.urlToImage} newsUrl={data.newsUrl}/>
+          </div>  
+          })}
+        </div>
       </div>
     )
   }
--- src/Components/NewsIteam.js
+++ src/Components/NewsIteam.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 
 export default class NewsIteam extends Component {
   render() {
-    let {title , descripation} = this.props;
+    let { title, descripation,imageUrl,newsUrl } = this.props;
     return (
-      <div>
-        <div className="card" style="width: 18rem;">
-          <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{descripation}</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
+      <div className='container my-3'>
+        <div className="card" style={{width: "100%"}}>
+          <img src={imageUrl} width={"100%"} height={"200vh"}  className="card-img-top object-fit-cover" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{descripation}...</p>
+            <a href={newsUrl}  className="btn btn-sm btn-primary">Read more</a>
+          </div>
         </div>
       </div>
     )
