96df9170d22cfc04627610963587e542975992f9
yadavvikas787840@gmail.com <yadavvikas787840@gmail.comcls>
2025-04-22 15:39:26 +0530

    add next page

--- src/Components/News.js
+++ src/Components/News.js
@@ -13,12 +13,34 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-  let url = "https://newsapi.org/v2/everything?q=tesla&from=2025-03-21&sortBy=publishedAt&apiKey=c6858b56451a434584657d39a5ad23a7";
+  let url = "https://newsapi.org/v2/everything?q=tesla&from=2025-03-22&sortBy=publishedAt&apiKey=c6858b56451a434584657d39a5ad23a7&page=1";
   
   let data = await fetch(url);
   let parsData = await data.json()
   this.setState({ articles: parsData.articles })
 }
+
+handdlePrevBtn = async () =>{
+  console.log("pre");
+  let url = `https://newsapi.org/v2/everything?q=tesla&from=2025-03-22&sortBy=publishedAt&apiKey=c6858b56451a434584657d39a5ad23a7&page=${this.state.page - 1}`;
+  let data = await fetch(url);
+  let parsData = await data.json()
+  this.setState({
+    page: this.state.page - 1,
+    articles: parsData.articles 
+  })
+}
+handdleNextBtn = async () =>{
+  console.log("next");
+  let url = `https://newsapi.org/v2/everything?q=tesla&from=2025-03-22&sortBy=publishedAt&apiKey=c6858b56451a434584657d39a5ad23a7&page=${this.state.page + 1}`;
+  let data = await fetch(url);
+  let parsData = await data.json()
+  this.setState({
+    page: this.state.page + 1,
+    articles: parsData.articles 
+  })
+}
+
 render() {
   return (
     <div className='container my-3'>
@@ -29,6 +51,10 @@ render() {
             <NewsIteam title={data.title ? data.title.slice(0, 40) : ""} descripation={data.description ? data.description.slice(0, 88) : ""} imageUrl={data.urlToImage} newsUrl={data.url} />
           </div>
         })}
+      </div>
+      <div class="d-flex justify-content-between">
+      <button type="button" disabled={this.state.page<=1} onClick={this.handdlePrevBtn} className="btn btn-dark">Prev</button>
+      <button type="button" onClick={this.handdleNextBtn} className="btn btn-dark">Next</button>
       </div>
     </div>
   )
